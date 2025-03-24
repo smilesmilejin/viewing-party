d59de1a012045d39ea345a43833ef4c6e9e24e19
Xinshuang Jin <jinxinshuang@gmail.com>
2025-03-24 14:46:37 -0400

    Finished wave1 and passed the test1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,26 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if (not title) or (not genre) or (not rating):
+        return None
+    movie_dict = {"title": title, "genre": genre, "rating": rating}
+    return movie_dict
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+
+    for movie in user_data["watchlist"]:
+        if title == movie["title"]:
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(movie)
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
